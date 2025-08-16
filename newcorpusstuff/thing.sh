3de2db1d18da2f1c8cd62ff639a381cbd4ced15d
personnumber3377 <sontapaa.jokulainen@gmail.com>
2025-08-16 22:54:25 +0300

    Blah

---
+++ newcorpusstuff/thing.sh
@@ -0,0 +1,5 @@
+wget -r -np -nH --cut-dirs=3 -R "index.html*" \
+  https://people.sc.fsu.edu/~jburkardt/data/ps/
+wget -r -np -nH --cut-dirs=3 -R "index.html*" \
+  https://people.sc.fsu.edu/~jburkardt/data/eps/
+
